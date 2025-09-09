c96359523584b676d6a1f25b57e73e0eab1680a3
sweder25 <ji.maldonado@duocuc.cl>
2025-09-08 23:22:47 -0300

    Actualización, front, back

---
+++ LvLUp-Backend/src/main/java/com/lvlup/LvLUp/LvLUpApplication.java
@@ -0,0 +1,13 @@
+package com.lvlup.LvLUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LvLUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LvLUpApplication.class, args);
+	}
+
+}
